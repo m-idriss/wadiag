f4c5806b5da932796b620fa9453c5ac05bb510b5
Idriss MOHAMADY <pro.mohamady@gmail.com>
2023-10-11 01:47:54 +0200

    [idm-0000] first commit

---
+++ src/main/java/com/dime/wadiag/WadiagApplication.java
@@ -0,0 +1,13 @@
+package com.dime.wadiag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WadiagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WadiagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dime/wadiag/WadiagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dime.wadiag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WadiagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
