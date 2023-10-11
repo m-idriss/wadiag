f1c47ecf4078bf6a74608bc4761d247e01b00085
Idriss MOHAMADY <pro.mohamady@gmail.com>
2023-10-11 13:48:16 +0200

    [idm-0000] log, banner, properties, SpringBootApplication

--- src/main/java/com/dime/wadiag/WadiagApplication.java
+++ src/main/java/com/dime/wadiag/WadiagApplication.java
@@ -1,13 +1,42 @@
 package com.dime.wadiag;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@Slf4j
 public class WadiagApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WadiagApplication.class, args);
-	}
+	public static void main(String[] args) throws UnknownHostException {
+	    var app = new SpringApplication(WadiagApplication.class);
+	    final Environment env = app.run(args).getEnvironment();
+	    var protocol = "http";
+	    if (env.getProperty("server.ssl.key-store") != null) {
+	      protocol = "https";
+	    }
+	    log.info("\n");
+	    log.info("""
+	        
+	         ----------------------------------------------------------
+	        \t Application '{}' is running!
+	        \t Access URLs:
+	        \t - Local: \t{}://localhost:{}
+	        \t - External: \t{}://{}:{}
+	        \t Sawgger UI: \t{}://localhost:{}/swagger-ui.html
+	        \t Profile(s): \t{}
+	         ----------------------------------------------------------
+	        """,
+	        //
+	        env.getProperty("spring.application.name"),
+	        protocol, env.getProperty("server.port"),
+	        protocol, InetAddress.getLocalHost().getHostAddress(), env.getProperty("server.port"),
+	        protocol, env.getProperty("server.port"),
+	        env.getActiveProfiles());
+	  }
 
 }
