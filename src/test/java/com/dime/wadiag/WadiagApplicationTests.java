556119e7ece57f7d0117ad7fa5eb4ba73560768c
Idriss <44480885+m-idriss@users.noreply.github.com>
2023-10-15 00:13:06 +0200

    Develop (#10)
    * [idm-0000] update banner
    
    * Create README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Merge master into develop (#9)
    
    * Bump com.h2database:h2 from 1.4.200 to 2.2.220
    
    Bumps [com.h2database:h2](https://github.com/h2database/h2database) from 1.4.200 to 2.2.220.
    - [Release notes](https://github.com/h2database/h2database/releases)
    - [Commits](https://github.com/h2database/h2database/compare/version-1.4.200...version-2.2.220)
    
    ---
    updated-dependencies:
    - dependency-name: com.h2database:h2
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Create codeql.yml
    
    * Create SECURITY.md
    
    * Develop (#8)
    
    * [idm-0000] update banner
    * Create README.md
    
    ---------
    
    Co-authored-by: Idriss MOHAMADY <pro.mohamady@gmail.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Idriss MOHAMADY <pro.mohamady@gmail.com>
    
    * [idm-0000] add context test
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Idriss MOHAMADY <pro.mohamady@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/test/java/com/dime/wadiag/WadiagApplicationTests.java
+++ src/test/java/com/dime/wadiag/WadiagApplicationTests.java
@@ -1,13 +1,34 @@
 package com.dime.wadiag;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.UnknownHostException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
 
 @SpringBootTest
 class WadiagApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    @DisplayName("Test main")
+    void testMain() throws UnknownHostException {
+    	WadiagApplication.main(new String[]{});
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("Test load context")
+    void contextLoads() {
+        assertThat(context).isNotNull();
+    }
 
 }
