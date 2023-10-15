64f5c72aecd1c6969d9f29394ad6b3d4d142e15e
Idriss MOHAMADY <pro.mohamady@gmail.com>
2023-10-15 03:30:47 +0200

    Merge branch 'develop'

---
+++ src/test/java/com/dime/wadiag/configuration/LogbackConfigurationTest.java
@@ -0,0 +1,58 @@
+package com.dime.wadiag.configuration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.read.ListAppender;
+
+class LogbackConfigurationTest {
+	private LoggerContext context;
+	private ListAppender<ILoggingEvent> listAppender;
+
+	@BeforeEach
+	void setup() throws JoranException {
+		// Initialize the Logback logger context
+		context = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+		// Configure Logback with your XML configuration file
+		JoranConfigurator configurator = new JoranConfigurator();
+		configurator.setContext(context);
+		context.reset();
+		configurator.doConfigure("src/main/resources/logback-spring.xml");
+
+		// Create and add a ListAppender to capture log output
+		listAppender = new ListAppender<ILoggingEvent>();
+		listAppender.start();
+		Logger logger = context.getLogger("com.dime");
+		logger.addAppender(listAppender);
+	}
+
+	@AfterEach
+	void tearDown() {
+		// Remove the ListAppender and reset the context after each test
+		Logger logger = context.getLogger("com.dime");
+		logger.detachAppender(listAppender);
+		listAppender.stop();
+		context.reset();
+	}
+
+	@Test
+	void testLogbackConfiguration() {
+		// Get a logger instance
+		Logger logger = context.getLogger("com.dime");
+		assertEquals("com.dime", logger.getName());
+		// Log some messages
+		logger.info("Test message");
+		logger.error("Error message");
+
+	}
+}
