4d27c5ba6af5628ec04adc092ae59765df015386
Idriss MOHAMADY <pro.mohamady@gmail.com>
2023-10-15 02:21:37 +0200

    [idm-0000] format

--- src/main/java/com/dime/wadiag/WadiagApplication.java
+++ src/main/java/com/dime/wadiag/WadiagApplication.java
@@ -3,42 +3,40 @@ package com.dime.wadiag;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @Slf4j
 public class WadiagApplication {
 
 	public static void main(String[] args) throws UnknownHostException {
-	    var app = new SpringApplication(WadiagApplication.class);
-	    final Environment env = app.run(args).getEnvironment();
-	    var protocol = "http";
-	    if (env.getProperty("server.ssl.key-store") != null) {
-	      protocol = "https";
-	    }
-	    log.info("\n");
-	    
-	    final String SERVER_PORT = "server.port";
-		log.info("""
-				
-			----------------------------------------------------------
-			\t Application '{}' is running!
-			\t Access URLs:
-			\t - Local: \t{}://localhost:{}
-			\t - External: \t{}://{}:{}
-			\t Swagger UI: \t{}://localhost:{}/swagger-ui.html
-			\t Profile(s): \t{}
-			----------------------------------------------------------
-			""",
-	        //
-	        env.getProperty("spring.application.name"),
-	        protocol, env.getProperty(SERVER_PORT),
-	        protocol, InetAddress.getLocalHost().getHostAddress(), env.getProperty(SERVER_PORT),
-	        protocol, env.getProperty(SERVER_PORT),
-	        env.getActiveProfiles());
-	  }
+		var app = new SpringApplication(WadiagApplication.class);
+		final Environment env = app.run(args).getEnvironment();
+		var protocol = "http";
+		if (env.getProperty("server.ssl.key-store") != null) {
+			protocol = "https";
+		}
+		log.info("\n");
 
+		final String SERVER_PORT = "server.port";
+		log.info("""
+
+				----------------------------------------------------------
+				\t Application '{}' is running!
+				\t Access URLs:
+				\t - Local: \t{}://localhost:{}
+				\t - External: \t{}://{}:{}
+				\t Swagger UI: \t{}://localhost:{}/swagger-ui.html
+				\t Profile(s): \t{}
+				----------------------------------------------------------
+				""",
+				//
+				env.getProperty("spring.application.name"), protocol, env.getProperty(SERVER_PORT), protocol,
+				InetAddress.getLocalHost().getHostAddress(), env.getProperty(SERVER_PORT), protocol,
+				env.getProperty(SERVER_PORT), env.getActiveProfiles());
+	}
 }
--- src/test/java/com/dime/wadiag/WadiagApplicationTests.java
+++ src/test/java/com/dime/wadiag/WadiagApplicationTests.java
@@ -9,24 +9,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
-
 @SpringBootTest
 class WadiagApplicationTests {
 
-    @Autowired
-    private ApplicationContext context;
+	@Autowired
+	private ApplicationContext context;
 
-    @Test
-    @DisplayName("Test main")
-    void testMain() {
-        // Test the main method without throwing exceptions
-        Assertions.assertDoesNotThrow(() -> WadiagApplication.main(new String[]{}));
-    }
+	@Test
+	@DisplayName("Test main")
+	void testMain() {
+		// Test the main method without throwing exceptions
+		Assertions.assertDoesNotThrow(() -> WadiagApplication.main(new String[] {}));
+	}
 
-    @Test
-    @DisplayName("Test load context")
-    void contextLoads() {
-        assertThat(context).isNotNull();
-    }
+	@Test
+	@DisplayName("Test load context")
+	void contextLoads() {
+		assertThat(context).isNotNull();
+	}
 
 }
