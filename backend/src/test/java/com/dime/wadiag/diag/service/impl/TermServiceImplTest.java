8da7edfdfb1d0eede122e085d55fc8308fd282d5
Idriss MOHAMADY <pro.mohamady@gmail.com>
2023-12-18 22:56:51 +0100

    🎨 delete unused import

--- backend/src/test/java/com/dime/wadiag/diag/service/impl/TermServiceImplTest.java
+++ backend/src/test/java/com/dime/wadiag/diag/service/impl/TermServiceImplTest.java
@@ -19,7 +19,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.dime.wadiag.diag.model.Term;
