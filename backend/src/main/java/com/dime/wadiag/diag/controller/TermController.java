f83ec849224e6583f520702bbecaf02c27cd8cdb
Idriss MOHAMADY <pro.mohamady@gmail.com>
2023-12-30 01:20:14 +0100

    🎨 improve termController optional

--- backend/src/main/java/com/dime/wadiag/diag/controller/TermController.java
+++ backend/src/main/java/com/dime/wadiag/diag/controller/TermController.java
@@ -1,15 +1,16 @@
 package com.dime.wadiag.diag.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.io.IOException;
-import java.util.List;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +18,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import com.dime.wadiag.diag.exception.GenericError;
 import com.dime.wadiag.diag.model.Term;
@@ -28,7 +27,6 @@ import com.dime.wadiag.diag.service.TermService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import java.util.Collections;
 
 /*
  * https://www.codejava.net/frameworks/spring-boot/rest-api-crud-with-hateoas-tutorial
@@ -39,61 +37,51 @@ import java.util.Collections;
 @Tag(name = "Terms", description = "Manage Terms")
 public class TermController {
 
-  @Autowired
-  private TermService termService;
-
-  private TermModelAssembler modelAssembler;
+  private final TermService termService;
+  private final TermModelAssembler modelAssembler;
 
   @Operation(summary = "Find All Terms")
   @GetMapping
   public CollectionModel<EntityModel<Term>> listAll() {
-    List<Term> terms = termService.findAll().orElse(Collections.emptyList());
-
-    List<EntityModel<Term>> entityModels = terms.stream()
-        .map(modelAssembler::toModel)
-        .collect(Collectors.toList());
-
-    CollectionModel<EntityModel<Term>> collectionModel = CollectionModel.of(entityModels);
-    collectionModel.add(linkTo(methodOn(TermController.class).listAll()).withSelfRel());
-
-    return collectionModel;
+    return termService.findAll()
+        .map(terms -> terms.stream().map(modelAssembler::toModel).collect(Collectors.toList()))
+        .map(termModels -> CollectionModel.of(termModels)
+            .add(linkTo(methodOn(TermController.class).listAll()).withSelfRel()))
+        .orElse(CollectionModel.of(Collections.emptyList()));
   }
 
   @Operation(summary = "Get Term by ID")
   @GetMapping("/{id}")
-  public ResponseEntity<EntityModel<Term>> getOne(@PathVariable("id") Long id) {
-    Optional<Term> term = termService.findById(id);
-    if (term.isPresent()) {
-      EntityModel<Term> model = modelAssembler.toModel(term.get());
-      return new ResponseEntity<>(model, HttpStatus.OK);
-    }
-    throw GenericError.TERM_NOT_FOUND.exWithArguments(Map.of("id", id));
+  public ResponseEntity<EntityModel<Term>> getOne(@PathVariable Long id) {
+    return termService.findById(id)
+        .map(term -> ResponseEntity.ok(modelAssembler.toModel(term)))
+        .orElseThrow(() -> GenericError.TERM_NOT_FOUND.exWithArguments(Map.of("id", id)));
   }
 
   @Operation(summary = "Create Term")
   @PostMapping("/{word}")
   public ResponseEntity<EntityModel<Term>> createTerm(@PathVariable String word) throws IOException {
     String wordLower = word.toLowerCase();
+
     Optional<Term> existingTerm = termService.findByWord(wordLower);
     if (existingTerm.isPresent()) {
-      EntityModel<Term> model = modelAssembler.toModel(existingTerm.get());
-      return new ResponseEntity<>(model, HttpStatus.OK);
+      return ResponseEntity.ok(modelAssembler.toModel(existingTerm.get()));
     }
-    Term term = termService.create(wordLower)
+
+    return termService.create(wordLower)
+        .map(term -> ResponseEntity.created(linkTo(methodOn(TermController.class)
+            .getOne(term.getId())).toUri()).body(modelAssembler.toModel(term)))
         .orElseThrow(() -> GenericError.WORD_NOT_FOUND.exWithArguments(Map.of("word", wordLower)));
-    EntityModel<Term> model = modelAssembler.toModel(term);
-    return ResponseEntity.created(linkTo(methodOn(TermController.class)
-        .getOne(term.getId())).toUri()).body(model);
   }
 
   @Operation(summary = "Delete Term by Word")
   @DeleteMapping("/{word}")
   public ResponseEntity<Void> deleteTerm(@PathVariable String word) {
     String wordLower = word.toLowerCase();
-    Optional<Integer> count = termService.deleteByWord(wordLower);
-    if (count.isPresent() && count.get() > 0) {
-      return ResponseEntity.noContent().<Void>build();
-    }
-    throw GenericError.WORD_NOT_FOUND.exWithArguments(Map.of("word", wordLower));
+
+    return termService.deleteByWord(wordLower)
+        .filter(deleteCount -> deleteCount > 0)
+        .map(d -> ResponseEntity.noContent().<Void>build())
+        .orElseThrow(() -> GenericError.WORD_NOT_FOUND.exWithArguments(Map.of("word", wordLower)));
   }
 }
