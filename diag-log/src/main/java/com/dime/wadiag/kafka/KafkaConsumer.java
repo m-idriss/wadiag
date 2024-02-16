5bfbff35bc593f365624947e52e566562e66c75a
Idriss MOHAMADY <pro.mohamady@gmail.com>
2024-02-16 23:52:44 +0100

    🐛 [feature/diag-log] save message value

--- diag-log/src/main/java/com/dime/wadiag/kafka/KafkaConsumer.java
+++ diag-log/src/main/java/com/dime/wadiag/kafka/KafkaConsumer.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import com.dime.wadiag.model.LogModel;
 import com.dime.wadiag.service.LogService;
 
+import io.micrometer.common.util.StringUtils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,11 +22,15 @@ public class KafkaConsumer {
 
     @KafkaListener(topics = KafkaConstants.TOPIC, groupId = KafkaConstants.GROUP_ID)
     public void listenGroupFoo(String message) {
+        if (StringUtils.isEmpty(message)) {
+            return;
+        }
         log.info("Received Message in group " + KafkaConstants.GROUP_ID + " => " + message);
+        String word = message.split("\\.")[2];
         service.save(LogModel.builder()
                 .httpStatus(HttpStatus.OK.value())
-                .content(message.split("\\.")[2])
-                .message(message)
+                .content(word)
+                .message(message.substring(0, message.length() - word.length() - 1))
                 .topic(KafkaConstants.TOPIC)
                 .detail(KafkaConstants.GROUP_ID)
                 .build());
