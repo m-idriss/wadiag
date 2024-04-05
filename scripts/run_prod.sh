daff1837cfc161187041611696accb30fb706194
Idriss MOHAMADY <pro.mohamady@gmail.com>
2024-04-05 21:48:37 +0200

    ✏️ [master] change to docker compose V2

--- scripts/run_prod.sh
+++ scripts/run_prod.sh
@@ -5,7 +5,7 @@ git reset --hard origin/master
 git pull origin master
 
 # Stop and remove existing containers
-docker-compose down
+docker compose down
 
 # Start containers with production environment variables
-docker-compose --env-file=.env.prod up --build -d
+docker compose --env-file=.env.prod up --build -d
